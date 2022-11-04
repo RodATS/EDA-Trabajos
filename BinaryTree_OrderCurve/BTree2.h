537d02b342573949f256ce1d07e047bdd8b8a50d
RodATS <77297145+RodATS@users.noreply.github.com>
2022-11-03 21:02:24 -0600

    Create BTree2.h

---
+++ BinaryTree_OrderCurve/BTree2.h
@@ -0,0 +1,173 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include <stdlib.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct node {
+  int data;
+  int hijos=0;
+  struct node *left;
+  struct node *right;
+};
+
+// New node creation
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+void Insert(struct node *r,int data){
+  if(r->hijos==0){
+    r->left = newNode(data);
+    r->hijos++;
+    return;
+  }
+  if(r->hijos==1)
+    {
+    r->right = newNode(data);
+    r->hijos++;
+    return;
+  }
+
+  //descender
+  if(r->hijos==2){
+    if(r->left->hijos < 2){
+      Insert(r->left,data); return ;
+      }
+    
+    if(r->left->hijos >= 2 and r->right->hijos < 2)
+      { Insert(r->right,data); return ;
+      }
+    Insert(r->left,data); return ;
+  }
+  
+}
+
+
+
+
+vector<int> PrO;
+vector<int> IO;
+vector<int> PsO;
+//Impresiones completas
+// Traverse Preorder
+void traversePreOrder(struct node *temp) {
+  if (temp != NULL) {
+    cout << " " << temp->data;
+    PrO.push_back(temp->data);
+    traversePreOrder(temp->left);
+    traversePreOrder(temp->right);
+  }
+}
+
+// Traverse Inorder
+void traverseInOrder(struct node *temp) {
+  if (temp != NULL) {
+    traverseInOrder(temp->left);
+    cout << " " << temp->data;
+    IO.push_back(temp->data);
+    traverseInOrder(temp->right);
+  }
+}
+
+// Traverse Postorder
+void traversePostOrder(struct node *temp) {
+  if (temp != NULL) {
+    traversePostOrder(temp->left);
+    traversePostOrder(temp->right);
+    cout << " " << temp->data;
+    PsO.push_back(temp->data);
+  }
+}
+
+
+
+//Busqueda
+
+// Traverse Preorder
+void traversePreOrderInd(struct node *temp, int x, int y) {
+  if (temp != NULL) {
+    for(int i=0;i<PrO.size();i++)
+    if(PrO[i] == x){
+      cout << " " << PrO[i];
+      x=-1;
+    }
+    else{
+        if(PrO[i] == y){
+          cout << " " << PrO[i];
+          y=-1;
+        }
+  
+        else {
+          if(y==-1 and x==-1)
+            return;
+          if(y==-1 or x==-1)
+            cout << " " << PrO[i];
+          }
+      }
+     
+  }
+}
+
+// Traverse Inorder
+void traverseInOrderInd(struct node *temp,int x, int y) {
+  if (temp != NULL) {
+    for(int i=0;i<IO.size();i++)
+    if(IO[i] == x){
+      cout << " " << IO[i];
+      x=-1;
+    }
+    else{
+        if(IO[i] == y){
+          cout << " " << IO[i];
+          y=-1;
+        }
+  
+        else {
+          if(y==-1 and x==-1)
+            return;
+          if(y==-1 or x==-1)
+            cout << " " << IO[i];
+          }
+          
+      }
+     
+  }
+}
+
+
+
+// Traverse Postorder
+void traversePostOrderInd(struct node *temp,int x, int y) {
+  if (temp != NULL) {
+   for(int i=0;i<PsO.size();i++)
+    if(PsO[i] == x){
+      cout << " " << PsO[i];
+      x=-1;
+    }
+    else{
+        if(PsO[i] == y){
+          cout << " " << PsO[i];
+          y=-1;
+        }
+  
+        else {
+          if(y==-1 and x==-1)
+            return;
+          if(y==-1 or x==-1)
+            cout << " " << PsO[i];
+          }
+      }
+  }
+}
+#endif
