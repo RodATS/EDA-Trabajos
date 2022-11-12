9a0fc482f19020199d884f518b674276a0b19130
Fabian2727 <80995242+Fabian2727@users.noreply.github.com>
2022-11-11 21:02:18 -0500

    Update main.cpp

--- K_means/main.cpp
+++ K_means/main.cpp
@@ -1,23 +1,70 @@
-
-#include "omp.h"
-#include <algorithm>
 #include <cmath>
-#include <fstream>
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <vector>
-#include <list>
-
-//Otro código en C++
-// https://codereview.stackexchange.com/questions/191081/k-means-image-segmentation-algorithm
 
 using namespace std;
 
+// Estructura para RGB
 struct pixel {
   unsigned char R;
   unsigned char G;
   unsigned char B;
 };
 
+// Cálculo de distancia hacia los centroides
+float distancia(pixel a, pixel b) {
+  float r_ = pow((int)a.R - (int)b.R, 2);
+  float b_ = pow((int)a.B - (int)b.B, 2);
+  float g_ = pow((int)a.G - (int)b.G, 2);
+  float dist = sqrt(r_ + b_ + g_);
+  return dist;
+}
+
+// Verificacion de igualdad de centroides
+bool compare(pixel a, pixel b) {
+  if (a.R == b.R && a.B == b.B && a.G == b.G)
+    return true;
+  return false;
+}
+
+// Verificar todos los cemtroides
+bool compare_all(vector<pixel> c, vector<pixel> caux) {
+  for (int i = 0; i < caux.size(); i++) {
+    if (!compare(c[i], caux[i]))
+      return false;
+  }
+  return true;
+}
+
+// Crear pixel vacío
+pixel p_null() {
+  pixel null;
+  null.R = (unsigned char)1;
+  null.G = (unsigned char)1;
+  null.B = (unsigned char)1;
+  return null;
+}
+
+void promedio(pixel &n, vector<pixel *> aux) {
+  int r, g, b;
+  for (int i = 0; i < aux.size(); i++) {
+    r += (int)(*aux[i]).R;
+    g += (int)(*aux[i]).G;
+    b += (int)(*aux[i]).B;
+  }
+  r = r / aux.size();
+  b = b / aux.size();
+  g = g / aux.size();
+  n.R = (unsigned char)r;
+  n.G = (unsigned char)g;
+  n.B = (unsigned char)b;
+}
+
+// Porcesado de image --> Read, Write
 struct img {
   int width = 0, height = 0;
   int rowstep = 0;
@@ -61,11 +108,9 @@ struct img {
         rgb_data[i][count].R = data[j];
         rgb_data[i][count].G = data[j + 1];
         rgb_data[i][count].B = data[j + 2];
-        cout<<rgb_data[i][count].R <<"\t"<<rgb_data[i][count].G<<"\t"<<rgb_data[i][count].B<<endl;
         count++;
       }
     }
-    cout<<endl;
     fclose(f);
   }
 
@@ -80,7 +125,6 @@ struct img {
         data[j] = rgb_data[i][j / 3].B;
         data[j + 1] = rgb_data[i][j / 3].G;
         data[j + 2] = rgb_data[i][j / 3].R;
-       // cout<<rgb_data[i][j / 3].R<<"\t"<<rgb_data[i][j / 3].G<<"\t"<<rgb_data[i][j / 3].B<<endl;
       }
       fwrite(data, sizeof(unsigned char), rowstep, aux);
       free(data);
@@ -88,153 +132,69 @@ struct img {
     fclose(aux);
   }
 
-
-
-//formula distancia
-  float distancia(pixel a, pixel b){
-    return sqrt(pow((float)a.R - (float)b.R,2)+pow((float)a.G-(float)b.G,2)+pow((float)a.B-(float)b.B,2));
-  }
-
-//saber a que centroide pertenece cada pixel
-  void dis(pixel *k, pixel aux, list<pixel> *dat, int n){
-    float prom=distancia(k[0],aux);
-    cout<<"prueba"<<endl;
-    cout<<prom<<endl;
-    int indice=0;
-    for(int i=1;i<n;i++){
-      cout<<distancia(k[i],aux)<<endl;
-      if(distancia(k[i],aux) < prom){
-        cout<<distancia(k[i],aux)<<endl;
-        indice = i;
+  // Procesado de datos, y clusterización para la segmentación de la IMG
+  void k_means(int clusters, int attempts = 20) {
+    vector<pixel> centroides(clusters);
+    vector<pixel> caux(clusters);
+    vector<vector<pixel *>> groups(clusters);
+    int index;
+    for (int i = 0; i < clusters; i++) {
+      centroides[i] = rgb_data[rand() % height][rand() % width];
+      if (i > 0 && compare(centroides[i], centroides[i - 1]))
+        centroides[i] = rgb_data[rand() % height][rand() % width];
+      caux[i] = p_null();
+    }
+    bool igual = false;
+    while (attempts--) {
+      for (int c = 0; c < clusters; c++) {
+        if (!groups[c].empty())
+          groups[c].erase(groups[c].begin(),
+                          groups[c].begin() + groups[c].size());
       }
-    }
-    cout<<(aux.R)<<"\t"<<aux.G<<"\t"<<aux.B<<endl;
-    dat[indice].push_back(aux);
-    return;
-  }
 
-
-
-//saber si el centroide actual es igual anterior
-  bool comparar(pixel *k, pixel *k_ant, int n){
-    bool res=true;
-    for(int i=0;i<n;i++){
-      if(k[i].R != k_ant[i].R && k[i].G != k_ant[i].G && k[i].B != k_ant[i].B)
-        res=false;
-    }
-    return res;
-  }
-
-
-
-
-  void calculo_kmeans(pixel *k, pixel *k_ant, int n){
-    
-    list<pixel> *dat;
-    dat = new list<pixel>[n];
-
-    //colocar cada pixel en el centroide que le corresponde
-    for(int i=0;i<height; i++){
-      for(int j=0;j<width; j++){
-        dis(k,rgb_data[i][j],dat, n);
-         /*float prom=distancia(k[0],rgb_data[i][j]);
-          int indice=0;
-          for(int i=1;i<n;i++){
-            if(distancia(k[i],rgb_data[i][j]) < prom){
-              indice = i;
+      for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+          float min = 99999;
+          for (int c = 0; c < clusters; c++) {
+            float aux = distancia(centroides[c], rgb_data[i][j]);
+            if (aux < min) {
+              min = aux;
+              index = c;
             }
           }
-        cout<<indice<<endl;
-          dat[indice].push_back(rgb_data[i][j]);
-        */
-      }
-    }
-
-    //hallar los nuevos centroides
-    for(int i=0;i<n;i++){
-      float promR, promG, promB;int count;
-      cout<<"\t"<<i<<"\t: "<<dat[i].size()<<endl;
-      list<pixel> :: iterator j;
-      for(j=dat[i].begin(); j != dat[i].end();j++){
-        promR += (float)(*j).R;
-        promG += (float)(*j).G;
-        promB += (float)(*j).B;
-        count++;
+          groups[index].push_back(&rgb_data[i][j]);
         }
-      k[i].R = (unsigned char) promR/count;
-      k[i].G = (unsigned char) promG/count;
-      k[i].B = (unsigned char) promB/count;
-      
-      
-      
       }
+      int cont = 0;
+      for (int i = 0; i < clusters; i++) {
+        caux[i] = centroides[i];
+        promedio(centroides[i], groups[i]);
 
-    
-    if(comparar(k, k_ant,n)==true){
-      //reemplazar cada valor del pixel por sus centroides respectivos
-       for(int i=0;i<n;i++){
-      list<pixel> :: iterator j;
-      for(j=dat[i].begin(); j != dat[i].end();j++){
-        (*j).R = k[i].R;
-        (*j).G = k[i].G;
-        (*j).B = k[i].B;
+        if (compare(caux[i], centroides[i])) {
+          cont++;
         }
-     /* cout<<"lo encontrók"<<endl;
-         cout<<k[i].R<<"\t"<<k[i].G<<"\t"<<k[i].B<<endl;
-         cout<<"matr"<<endl;
-         cout<<(*j).R<<"\t"<<(*j).G<<"\t"<<(*j).B<<endl;*/
       }
-      return;
+      if (cont == clusters)
+        break;
     }
-    else{
-      
-      //dat.clear(); //borrar los elementos del centroide 
-      //creo que crear la lista con new no es necesario 
-      //pero para borrarla?
-      for(int i=0;i<n;i++){
-        //dat[i].clear();// borrar los elementos de cada centroide
-        cout<<"k"<<endl;
-         cout<<k_ant[i].R<<"\t"<<k_ant[i].G<<"\t"<<k_ant[i].B<<endl;
-      
-        k_ant[i].R = k[i].R ;
-        k_ant[i].G = k[i].G ;
-        k_ant[i].B = k[i].B ;
-        cout<<"k_ant"<<endl;
-         cout<<k_ant[i].R<<"\t"<<k_ant[i].G<<"\t"<<k_ant[i].B<<endl;
-      }
-        
-      
-      calculo_kmeans(k,k_ant, n);
 
+    for (int c = 0; c < clusters; c++) {
+      if (!groups[c].empty()) {
+        for (int i = 0; i < groups[c].size(); i++) {
+          (*groups[c][i]) = centroides[c];
+
+          /*(*groups[c][i]).R = (centroides[c]).R;
+          (*groups[c][i]).G = (centroides[c]).G;
+          (*groups[c][i]).B = (centroides[c]).B;*/
+        }
+      }
     }
-    
-    return;
-  }
-
-//aca inicia
-  void kmeans(int k){
-    pixel *ks= new pixel[k];//los centroides
-    pixel *kant= new pixel[k];//los centroides para saber si son iguales y no se mueven
-    
-    for(int i=0;i<k;i++){
-      ks[i].R = (unsigned char)(rand() % 255);
-      ks[i].G = (unsigned char)(rand() % 180);
-      ks[i].B = (unsigned char)(rand() % 200);
-      }
-    calculo_kmeans(ks,kant, k);
-
-    
-    
-    cout<<"listo"<<endl;
-
-    write();
   }
 };
 
-
 int main() {
-  std::cout << "Hello World!\n";
-  char *filename = "test3.bmp";
-  img mario(filename);
-  mario.kmeans(3);
+  char *filename = "image.bmp";
+  img prueba(filename);
+  prueba.k_means(2);
+  //prueba.write();
 }
